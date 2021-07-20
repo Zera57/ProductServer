962b407d55a039d725278a1cf8e08e744f9dfbae
Avtonomov Svyatoslav <svetik.avtonomov@gmail.com>
2021-07-20 19:11:01 +0300

    Initial commit

---
+++ src/main/java/com/Zera57/ServerJPA/ServerJpaApplication.java
@@ -0,0 +1,13 @@
+package com.Zera57.ServerJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Zera57/ServerJPA/ServerJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zera57.ServerJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
