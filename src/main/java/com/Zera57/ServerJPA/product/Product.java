59d93fc8906333027b3eec568aefd466baa14530
Avtonomov Svyatoslav <svetik.avtonomov@gmail.com>
2021-07-20 19:25:31 +0300

    Maded model of Product

---
+++ src/main/java/com/Zera57/ServerJPA/product/Product.java
@@ -0,0 +1,70 @@
+package com.Zera57.ServerJPA.product;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private Integer cost;
+    private Integer count;
+
+    public Product(String name,
+                   String description,
+                   Integer cost,
+                   Integer count) {
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+        this.count = count;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", cost=" + cost +
+                ", count=" + count +
+                '}';
+    }
+}
